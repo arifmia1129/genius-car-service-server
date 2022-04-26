93fe4ffc849183ee1f0492ae03f68ab96397922b
arifvtti <arif.vtti@gmail.com>
2022-04-27 05:36:15 +0600

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+function verifyAuth(req, res, next) {
+    const reqAuth = req.headers.authorization;
+    if (!reqAuth) {
+        res.status(401).send({ message: "Unauthorize!" })
+    }
+    const token = reqAuth.split(" ")[1];
+    console.log(token);
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            res.status(403).send({ message: "Forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apkoi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -18,7 +36,8 @@ async function run() {
         await client.connect();
 
         const servicesCollection = client.db("geniusCar").collection("service");
-
+        const orderCollection = client.db("geniusCar").collection("order");
+        // Service
         app.get("/service", async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
@@ -48,6 +67,37 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await servicesCollection.deleteOne(query);
             res.send(result);
+        })
+
+        // Order
+        app.get("/order", verifyAuth, async (req, res) => {
+
+            const decodedEmail = req?.decoded?.user;
+            const email = req?.query?.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+
+            else {
+                res.status(403).send({ message: "Forbidden access." })
+            }
+
+        })
+
+        app.post("/order", async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+        app.post("/login", async (req, res) => {
+            const user = req.body.address;
+            const token = jwt.sign({ user }, process.env.SECRET_KEY, {
+                expiresIn: "1d"
+            });
+            res.send({ token });
         })
     }
 
